b0ceaa1b1e8e1ed9a567e2db11a7ba9c9a58dd0b
souemon16 <souemon16@gmail.com>
2020-08-27 23:56:33 +0600

    2nd commit

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -5,6 +5,15 @@ import "./Shop.css";
 
 const Shop = (props) => {
     const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />
+
+    const cart = props.cart;
+    const total = cart.reduce((total, course) => total + course.price, 0);
+
+    // Format Number 
+    function formatNumber(num) {
+        const precision = num.toFixed(2);
+        return Number(precision);
+    }
     return (
         <div id="shop">
             <div className="shop-course">
@@ -12,7 +21,10 @@ const Shop = (props) => {
                 <p>Choose from 150,000 online video courses with new additions published every month</p>
             </div>
             <div className="shop-cart">
-                <button className="cart btn btn-warning">{cartIcon}: {props.cart.length}</button>
+                <div>
+                    <p>Total Items{cartIcon}: {props.cart.length}</p>
+                    <p>Total Price: {total}</p>
+                </div>
             </div>
         </div>
     );
